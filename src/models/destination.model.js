8e03ae14e16f6cb31124ee48a66d89bc5013c0f8
unknown <64825962+D1urga@users.noreply.github.com>
2024-06-04 18:52:47 +0530

    updated

--- src/models/destination.model.js
+++ src/models/destination.model.js
@@ -15,7 +15,10 @@ const destinationSchema = new mongoose.Schema(
       type: String,
       required: true,
     },
-
+    cityTags: {
+      type: String,
+      required: true,
+    },
     originalCost: {
       type: String,
       required: true,
@@ -24,19 +27,47 @@ const destinationSchema = new mongoose.Schema(
       type: String,
       required: true,
     },
-    hotelOptions: {
-      type: String,
-      required: true,
-    },
-    icludedService: {
-      type: String,
-      required: true,
-    },
     duration: {
       type: String,
       required: true,
     },
     distance: {
+      type: String,
+      required: true,
+    },
+    ittitle: {
+      type: String,
+      required: true,
+    },
+    itdes: {
+      type: String,
+      required: true,
+    },
+    fivestar: {
+      type: String,
+      required: true,
+    },
+    fourstar: {
+      type: String,
+      required: true,
+    },
+    threestar: {
+      type: String,
+      required: true,
+    },
+    breakfast: {
+      type: String,
+      required: true,
+    },
+    transfers: {
+      type: String,
+      required: true,
+    },
+    sightseeing: {
+      type: String,
+      required: true,
+    },
+    watersports: {
       type: String,
       required: true,
     },
